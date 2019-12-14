318df991865794f75581ab1f823d6d2487768e86
Sabrina Settle <ssettle@e1z2r5p13.42.us.org>
2019-12-13 16:33:40 -0800

    While loop for date arrays setup

--- WaterMeTandem/calender.rb
+++ WaterMeTandem/calender.rb
@@ -1,13 +1,10 @@
 
+# The array is nested, so it parsed into two seprate types of arrays later, the names of names assoicated with durations 
+# and multiple arrarys containing the dates for watering for each duration.
 $info_objects = JSON.parse(File.read('Apprentice_WeGrowInTandem_Data.json'), object_class: OpenStruct)
 
 #get rid of later
 # pp $info_objects[1]
-
-
-
-
-#the array is nested
 
 
 #all watered on the Monday at start
@@ -31,9 +28,9 @@ $currentDay = DateTime.now
 
 #Time play
 t = Time.now
-puts day_date: t.day
-puts month: t.month
-puts hour: t.hour
+# puts day_date: t.day
+# puts month: t.month
+# puts hour: t.hour
 
 t = Time.now
 d = t.day
@@ -41,13 +38,72 @@ m = t.month
 y = t.year
 
 d = t.end_of_month.day
-puts day: d
+# puts day: d
 
 
 #I NEED THIS TO DO THE THINGS
 # str = "14 days"
 # puts duration_to_days(str)
 # i = duration_to_days(str);
+
+def ifWeekend(date)
+
+end
+
+# not gonna worry about the 1 week waterings or the 2 week ones as they wont fall on a weekend
+def ftDays(numOf, dur)
+  wateringDayFt = []
+  watering = $startDate
+  wateringDayFt << watering
+  while dur > 0
+    watering = watering + numOf.days
+    wateringDayFt << watering
+    dur = dur - numOf
+  end
+  # pp wateringDayFt
+end
+
+
+def sevenDays(numOf, dur)
+  wateringDaySev = []
+  watering = $startDate #+ numOf.days
+  wateringDaySev << watering
+  while dur > 0
+    watering = watering + numOf.days
+    wateringDaySev << watering
+    dur = dur - numOf
+  end
+  # pp wateringDaySev
+end
+
+
+def threeDays(numOf, dur)
+  wateringDayThr = []
+  watering = $startDate
+  wateringDayThr << watering
+  while dur > 0
+    watering = watering + numOf.days
+    wateringDayThr << watering
+    dur = dur - numOf
+  end
+  # pp wateringDayThr
+  # ifWeekend(array)
+end
+
+
+#these may fall on a weekend, so I can do my if d.cwday == 6 || 7 or d.saturday? d.sunday? 
+def twoDays(int)
+  wateringDayTw = []
+  watering = $startDate
+  wateringDayTw << watering
+  while dur > 0
+    watering = watering + numOf.days
+    wateringDayTw << watering
+    dur = dur - numOf
+  end
+  pp wateringDayTw
+  # ifWeekend(date)
+end
 
 def duration_to_days(string)
   items = string.split(' ')
@@ -64,6 +120,10 @@ def startParsing
     checkWaterAfter($info_objects[i], "2 days")
     i += 1
   end
+  ftDays(duration_to_days("14 Days"), $duration)
+  sevenDays(duration_to_days("7 Days"), $duration)
+  threeDays(duration_to_days("3 Days"), $duration)
+  twoDays(duration_to_days("2 Days"), $duration)
 end
 
 def checkWaterAfter(array, str)
@@ -77,58 +137,21 @@ def fillArrays(array, int)
   if int == 14
     plant_ft_arr = []
     plant_ft_arr << array.name
-    pp plant_ft_arr
-    ftDays(int)
+    # pp plant_ft_arr
+    # ftDays(int)
   end
   if int == 7
     plant_sev_arr = []
     plant_sev_arr << array.name
-    pp plant_sev_arr
-    sevenDays(int)
   end
   if int == 3
     plant_th_arr = []
     plant_th_arr << array.name
-    pp plant_th_arr
-    threeDays(int)
   end
   if int == 2
     plant_th_arr = []
     plant_th_arr << array.name
-    pp plant_th_arr
-    twoDays(int)
   end
-end
-
-def ifWeekend()
-
-end
-
-# not gonna worry about the 1 week waterings or the 2 week ones as they wont fall on a weekend
-def ftDays(int)
-  # for or while loop that while the duration of time in the main project 
-  # create dates for watering
-  # decrement down with int = whatever(14? 7?)
-  wateringDayFt = []
-#   watering = $startDate + 14.days
-end
-
-
-def sevenDays(int)
-  wateringDaySev = []
-end
-
-
-def threeDays(int)
-  wateringDayThr = []
-  ifWeekend()
-end
-
-
-#these may fall on a weekend, so I can do my if d.cwday == 6 || 7 or d.saturday? d.sunday? 
-def twoDays(int)
-  wateringDayTw = []
-  ifWeekend()
 end
 
 
